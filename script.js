d4bd945c24b16cd5d8045f02b1a2f95668e3a6fd
Nickesh-123K <nickesh2020@gmail.com>
2024-12-16 15:53:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function msg(){
+    alert("this is an alert msg")
+}
+
+function nic(){
+    var v= confirm("Are u sure?");  
+if(v==true){  
+alert("ok");  
+}  
+else{  
+alert("cancel");  
+}  
+}
+
+function np(){
+    var v= prompt("Who are you?");  
+alert("I am "+v);  
+}
